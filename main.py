c35fb97cbb0e4d83cc150117eed6d8915e994c56
AdiXsus <58894271+AdiXsus@users.noreply.github.com>
2024-03-23 10:05:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ WEBHOOK_URL = os.getenv(
 TARGET_USER_ID = int(os.getenv(
     'TARGET_USER_ID'))  # Pobierz ID użytkownika z zmiennych środowiskowych
 MESSAGE_SCHEDULE = {  # Harmonogram wysyłania wiadomości (godzina: treść wiadomości)
-    "20:00": "🟢 Open **Sydney** 21:00",
-    "05:00": "🔴 Close **Sydney** 05:00",
-    "00:00": "🟢 Open **Tokyo** 01:00",
-    "09:00": "🔴 Close **Tokyo** 10:00",
-    "08:00": "🟢 Open **London** 09:00",
-    "17:00": "🔴 Close **London** 17:00",
-    "12:00": "🟢 Open **New York** 12:00",
-    "21:00": "🔴 Close **New York** 22:00",
+    "20:00": {"message": "🟢 Open **Sydney** 21:00", "status": "Close New York 22:00"},
+    "05:00": {"message": "🔴 Close **Sydney** 06:00", "status": "Open London 09:00"},
+    "00:00": {"message": "🟢 Open **Tokyo** 01:00", "status": "Close Sydney 06:00"},
+    "09:00": {"message": "🔴 Close **Tokyo** 10:00", "status": "Open New York 13:00"},
+    "08:00": {"message": "🟢 Open **London** 09:00", "status": "Close Tokyo 10:00"},
+    "17:00": {"message": "🔴 Close **London** 17:00", "status": "Open Sydney 21:00"},
+    "12:00": {"message": "🟢 Open **New York** 13:00", "status": "Close London 18:00"},
+    "21:00": {"message": "🔴 Close **New York** 22:00", "status": "Open Tokyo 01:00"},
 }
 
 # Inicjalizacja klienta Discord z odpowiednimi intencjami
@@ -29,45 +29,53 @@ client = discord.Client(intents=intents)
 
 # Funkcja do wysyłania wiadomości przez webhook
 async def send_webhook_message(message):
-  async with aiohttp.ClientSession() as session:
-    async with session.post(WEBHOOK_URL, json={"content":
-                                               message}) as response:
-      pass
+    async with aiohttp.ClientSession() as session:
+        async with session.post(WEBHOOK_URL, json={"content":
+                                                       message}) as response:
+            pass
 
 
 # Funkcja do wysyłania wiadomości prywatnej
 async def send_private_message(user, message):
-  try:
-    await user.send(message)
-    print(f"Wysłano wiadomość do {user.name}")
-  except discord.errors.Forbidden:
-    print(f"Nie można wysłać wiadomości prywatnej do {user.name}")
+    try:
+        await user.send(message)
+        print(f"Wysłano wiadomość do {user.name}")
+    except discord.errors.Forbidden:
+        print(f"Nie można wysłać wiadomości prywatnej do {user.name}")
+
+
+# Funkcja zmieniająca status bota
+async def change_bot_status(status):
+    await client.change_presence(activity=discord.Game(name=status))
 
 
 # Funkcja sprawdzająca i wysyłająca wiadomość o konkretnej godzinie
 async def check_and_send_message():
-  await client.wait_until_ready()
-  target_user = await client.fetch_user(TARGET_USER_ID)
+    await client.wait_until_ready()
+    target_user = await client.fetch_user(TARGET_USER_ID)
 
-  # Wysyłanie wiadomości startowej przez webhook
-  await send_webhook_message("Bot został uruchomiony.")
+    # Wysyłanie wiadomości startowej przez webhook
+    await send_webhook_message("Bot został uruchomiony.")
 
-  while not client.is_closed():
-    now = datetime.now().strftime("%H:%M")
-    if now in MESSAGE_SCHEDULE:
-      message_content = MESSAGE_SCHEDULE[now]
-      await send_private_message(target_user, message_content)
-      await asyncio.sleep(
-          60
-      )  # Zabezpieczenie przed wielokrotnym wysłaniem w tej samej minucie
-    await asyncio.sleep(30)  # Sprawdzanie co 30 sekund
+    while not client.is_closed():
+        now = datetime.now().strftime("%H:%M")
+        if now in MESSAGE_SCHEDULE:
+            schedule = MESSAGE_SCHEDULE[now]
+            message_content = schedule["message"]
+            status_content = schedule["status"]
+            await send_private_message(target_user, message_content)
+            await change_bot_status(status_content)  # Zmiana statusu bota
+            await asyncio.sleep(
+                60
+            )  # Zabezpieczenie przed wielokrotnym wysłaniem w tej samej minucie
+        await asyncio.sleep(30)  # Sprawdzanie co 30 sekund
 
 
 # Event połączenia z Discordem
 @client.event
 async def on_ready():
-  print(f'Zalogowano jako {client.user.name}')
-  client.loop.create_task(check_and_send_message())
+    print(f'Zalogowano jako {client.user.name}')
+    client.loop.create_task(check_and_send_message())
 
 
 # Uruchomienie bota
